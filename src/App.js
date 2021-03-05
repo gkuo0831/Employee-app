946043722f44ecbce1a0ce296621215dd4482b0c
Grace Kuo <gkuo.github@gmail.com>
2021-03-04 17:48:50 -0700

    requiring DOM nodes in app.js

--- src/App.js
+++ src/App.js
@@ -3,17 +3,44 @@ import Wrapper from "./Wrapper";
 import Searchbar from "./Searchbar";
 import Header from "./Header";
 import "./App.css";
+import API from "./Utils/api";
 
-function App() {
-  return (
-    <div className="App">
-      <Wrapper>
+class App extends Component {
+  state = {
+    employees: [],
+    search: "",
+    allEmployees: [],
+  };
+
+  componentDidMount() {
+    this.listEmployees(
+      "?inc=name,picture,location,registered,email,phone,dob&nat=US&results=30"
+    );
+  }
+
+  render() {
+    return (
+      <div className="container-fluid">
         <Header />
-        <Searchbar />
-        <Main />
-      </Wrapper>
-    </div>
-  );
+        <Search />
+
+        <Wrapper>
+          {this.state.employees.map((employee) => (
+            <EmployeeCard
+              removeEmployee={this.removeEmployee}
+              id={this.state.employees.indexOf(employee)}
+              key={this.state.employees.indexOf(employee)}
+              name={`${employee.name.title} ${employee.name.first} ${employee.name.last}`}
+              email={employee.email}
+              phone={employee.phone}
+              image={employee.picture.medium}
+            />
+          ))}
+        </Wrapper>
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
